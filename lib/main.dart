e27c2436408a4774b1aa999ad5d0436876b7e215
Yousef Hanafy <74376063+YousefHanafy-SW-ENG@users.noreply.github.com>
2023-07-08 18:25:07 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Montserrat',
         primarySwatch: Colors.blue,
       ),
-      home: OnBoardingScreen(),
+      home: LoginScreen(),
     );
   }
 }
